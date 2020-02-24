624796f66b2080ce496d5d2c293a49f44ac5c107
d3rboyfresh123 <d3rboyfresh123@hotmail.com>
2020-02-24 02:22:02 -0500

    created file with BST  and Node class

---
+++ Problem_2c.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self,value):
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.value = value
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insertIter(self,value):
+        root = self.root
+        current = None
+        while(root != None): 
+            current = root             
+            if(value < root.value):
+                root = root.left
+            elif(value > root.value):
+                root = root.right
+        if(current == None):
+            self.root = Node(value)
+        elif(value < current.value):
+            leftChild = Node(value)
+            current.left = leftChild
+            leftChild.parent = current
+        else:
+            rightChild = Node(value)
+            current.right = rightChild
+            rightChild.parent = current
+
+tree = BST()
+values = [10,5,20,6,12,7,11,16,19,18,17]
+for i in values:
+    tree.insertIter(i)
+
+print(tree.root.right.value)
