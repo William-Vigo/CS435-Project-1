66f134c1638ae3d36dcbfd726c752a3ed5a20fcf
d3rboyfresh123 <d3rboyfresh123@hotmail.com>
2020-02-27 11:47:34 -0500

    finished Problem 5a and 5c

--- Question4-7/Problem_5.py
+++ Question4-7/Problem_5.py
@@ -1,15 +1,20 @@
-import BSTrecursive as BST
+import BSTrecursive as recBST
+import BSTiterative as iterBST
 import BBST as BalanceBST
 import Problem_3 as ArrayOfInts
 
 randomArray = ArrayOfInts.getRandomArray(10000)
 
-recBST = BST.BST()
+iterBST  = iterBST.BST()
+recBST = recBST.BST()
 iterBBST = BalanceBST.BBST()
 
 #Problem 5a
 for i in randomArray:
     recBST.insertRec(i)
-    #iterBBST.insertIter(i)
+    iterBBST.insertIter(i)
 
 #Problem 5c
+for x in randomArray:
+    iterBST.insertIter(x)
+    iterBBST.insertIter(x)
